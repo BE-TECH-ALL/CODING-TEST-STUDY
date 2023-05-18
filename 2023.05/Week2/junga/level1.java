59223b0f85c6956496531a66a8ee2531803ff3a9
junga970 <111635735+junga970@users.noreply.github.com>
2023-05-18 20:20:39 +0900

    5월 2주차 알고리즘 풀이

---
+++ 2023.05/Week2/junga/level1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        Queue<Integer>[] newBoard = new Queue[board.length + 1];
+
+        // Queue 구조로 보드 만들기
+        for (int i = 0; i < board.length; i++) {
+            newBoard[i + 1] = new LinkedList();
+            
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][i] != 0) {
+                    newBoard[i + 1].add(board[j][i]);
+                }
+            }
+        }
+        
+        // 바구니
+        Stack<Integer> basket = new Stack();
+        basket.push(-1);
+        
+        int result = 0;
+        for (int i = 0; i < moves.length; i++) {
+            int move = moves[i];
+            Integer doll = newBoard[move].poll();
+            
+            if (doll == null) {
+                continue;
+            }
+            
+            if (doll.equals(basket.peek())) {
+                basket.pop();
+                result += 2;
+            } else {
+                basket.push(doll);
+            }
+        }
+
+        return result;
+    }
+}
